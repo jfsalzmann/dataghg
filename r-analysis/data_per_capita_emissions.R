482aecfc57a0150596050d116a56b53cf76a20e2
Fabian <78969880+FaMe-1337@users.noreply.github.com>
2022-10-24 23:09:04 +0200

    added EU to PerCapAnalysis

--- r-analysis/data_per_capita_emissions.R
+++ r-analysis/data_per_capita_emissions.R
@@ -2,17 +2,23 @@ load("data-transf/data_base.RData")
 
 #summarize per capita emissions by country
 data_countries <- data_base %>% 
-  group_by(country,year,region_ar6_6,pop) %>% 
+  group_by(country,year,region_ar6_6,pop,EU) %>% 
   summarise(GHG_pc=sum(GHG_pc,na.rm=TRUE))  
 
 data_china<-data_countries %>% 
   filter(country=={{COUNTRY}}) %>% 
   rename(region = country)
 
+data_EU<-data_countries %>% 
+  filter(EU) %>% 
+  select(-country) %>%
+  mutate(region_ar6_6 = "EU")
 
 #to show chinas development in rleations to diffrent world regions
 
-data_regions <- data_countries %>%
+
+data_regions <- data_countries %>% 
+  rbind(data_EU) %>% #appending data for EU countries to show in plot
   mutate(pop=replace_na(pop,0)) %>% # check if zero transformation changes statitstics
   group_by(region_ar6_6,year) %>% 
   summarise(GHG_pc=weighted.mean(GHG_pc,pop,na.rm=TRUE)) %>% 
@@ -21,7 +27,8 @@ data_regions <- data_countries %>%
 
 # combining data frames
 
-china_and_regions <- rbind(data_regions, data_china) %>%
+china_and_regions <- data_regions %>% 
+  rbind(data_china) %>%
   filter(year <= {{YEAR}})
 
 
