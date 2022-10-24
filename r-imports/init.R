9ffb9c708190ece9a512face4cc6a5b1faca2c73
Joff <mail@jfsalzmann.com>
2022-10-24 15:44:19 +0200

    init

---
+++ r-imports/init.R
@@ -0,0 +1,19 @@
+rm(list = ls(envir = globalenv()),envir = globalenv())
+
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+
+p_load(magrittr)
+p_load(readxl)
+p_load(ggplot2)
+p_load(tidyverse)
+
+
+"%cin%" = function(x,y){str_detect(y,x)}
+"%xin%" = function(x,y){x %cin% y | y %cin% x}
+"%ni%" = Negate("%in%")
+"%nci%" = Negate("%cin%")
+"%nxi%" = Negate("%xin%")
+"%.%" = function(x,y){paste(x,y,sep = "")}
+cNA = as.character(NA)
+as.numeric.factor = . %>% as.numeric(levels(.))[.]
