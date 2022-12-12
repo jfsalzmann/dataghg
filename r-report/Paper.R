850274874415d8794dfed62bd6d559210cc8003a
Joff <mail@jfsalzmann.com>
2022-12-12 16:58:27 +0100

    latex

---
+++ r-report/Paper.R
@@ -0,0 +1,17 @@
+# This is the R code used in the Markdown document. 
+# It is organised in sections like setup-ex below, and will be
+# included in the Markdown main file using the section name.
+
+
+
+##################################################################################
+## ---- setup-ex ----
+
+source('r-imports/init.R')
+source('r-analysis/meta.R')
+
+
+
+##################################################################################
+## DATA PREPARATION
+## ---- data-prep ----
