b5670ba6939566b7e890560318fbe8227f6f7786
Fabian <78969880+FaMe-1337@users.noreply.github.com>
2022-12-12 13:49:09 +0100

    cumCo2

--- r-analysis/data_cumulative_emissions_1750_1970_2019_2060.R
+++ r-analysis/data_cumulative_emissions_1750_1970_2019_2060.R
@@ -1,6 +1,7 @@
 load("data-transf/data_hist_co2.RData")
 load("data-transf/data_base.RData")
 
+
 # cumulative co2 untill 1970
 data_co2_1750_EU <-data_hist_co2%>% 
     filter(EU) %>% 
@@ -16,35 +17,35 @@ data_co2_1750_US_CH<-data_hist_co2 %>%
 data_cco2_1750<-  rbind(data_co2_1750_US_CH,data_co2_1750_EU) %>% 
   filter(year <= 1970)
 
-view(data_cco2_1750)
+
 data_cco2_1750%<>%
   select(co2,year,country) %>%
-  rename("GHG" =co2)
+  mutate(GHG =co2)
   
 #combine with 1970 Data 
   
 data_ghg_1970_EU <-data_base%>% 
   filter(EU) %>% 
   group_by(year) %>% 
-  summarise(GHG=sum(GHG,na.rm=T)) %>% 
+  summarise(GHG=sum(GHG,na.rm=T),
+            co2=sum(CO2,na.rm=T)) %>% 
   mutate(country = "EU-27")
 
 data_cghg_1750_2019<-data_base %>% 
   filter(ISO %in% c("USA","CHN")) %>% 
-  select(country,year,GHG) %>% 
+  select(country,year,GHG,CO2) %>% 
   group_by(country,year) %>% 
-  summarise(GHG=sum(GHG,na.rm=T)) %>% 
+  summarise(GHG=sum(GHG,na.rm=T),
+            co2=sum(CO2,na.rm=T)) %>% 
   rbind(data_ghg_1970_EU,data_cco2_1750) %>%
   arrange(year) %>% 
   group_by(country) %>% 
-  mutate(cum_ghg = cumsum(coalesce(GHG, 0)) + GHG*0)
+  mutate(cum_ghg = cumsum(coalesce(GHG, 0)) + GHG*0,
+         cum_co2 = cumsum(coalesce(co2, 0)) + co2*0)
 
 
 
 view(data_cghg_1750_2019)
 
-# combine 1750 with 1970 data
-
-data_cghg_1750_to_2019 <- %>% 
 
   
